69042e1a9002363c021b464c7e00e039b36b230b
Prasath <100816296+prasath-23@users.noreply.github.com>
2023-07-27 10:24:13 +0530

    Delete Day 3 - 10/project directory

--- Day 3 - 10/project/src/main/java/com/billApp/project/Repositories/DueBillsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.billApp.project.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.billApp.project.Entities.DueBills;
-
-public interface DueBillsRepo extends JpaRepository< DueBills , Integer >{
-    
-}
